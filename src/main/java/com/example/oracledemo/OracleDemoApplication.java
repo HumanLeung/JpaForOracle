1d54acf56050be4ba8329ca9c77b52bf1d8c3e44
1261771 <1261771@mochiwang.com>
2021-09-18 21:27:43 +0800

    oracle

---
+++ src/main/java/com/example/oracledemo/OracleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.oracledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleDemoApplication.class, args);
+        System.out.println("haha");
+    }
+
+}
